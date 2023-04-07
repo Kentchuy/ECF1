a42a4f26a8bbe566328233884fd42ea515c77f75
Kentchuy <thomas.k.chau@outlook.fr>
2023-04-07 21:38:33 +0200

    L'API est passée !

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+// Executé après la sécurité de chargement
+
+// Partie burger
 /* Sélection des éléments HTML */
 let link = document.getElementById('link')
 let burger = document.getElementById('burger')
@@ -9,3 +13,40 @@ e.preventDefault()
 burger.classList.toggle('open')
 ul.classList.toggle('open')
 })
+// Fin partie burger
+
+// Partie API Maps
+/*
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of cordonnerie
+  const position = { lat: 47.11057, lng: -2.06892 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  // The map, centered at cordonnerie
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "MAP_CORDO",
+  });
+
+  // The marker, positioned at cordonnerie ===== ne fonctionne pas
+  const marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: "Cordonnerie",
+  });
+}
+
+initMap();
+// Fin partie API Maps
+
+});
